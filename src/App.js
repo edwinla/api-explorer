edc0680b83053eb4964c4df1ef8600eeb8d36b5c
laed37 <La.Edwin37@gmail.com>
2017-03-23 23:04:07 -0700

    Add basic fetch request

--- src/App.js
+++ src/App.js
@@ -3,15 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      url: "",
+      method: ""
+    };
+  }
+
+  updateFieldFor = (field) => {
+    return (e) => this.setState({[field]: e.target.value})
+  }
+
+  fetchData = () => {
+    const {url, method} = this.state;
+    const myRequest = new Request(url, {method: method});
+    fetch(myRequest)
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to API Explorer</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <label>
+            url
+            <input
+              type="text"
+              onChange={this.updateFieldFor('url')}
+              value={this.state.url}
+            />
+          </label>
+          <label>
+            method
+            <input
+              type="text"
+              onChange={this.updateFieldFor('method')}
+              value={this.state.method}
+            />
+          </label>
+          <button onClick={this.fetchData}>submit</button>
         </p>
       </div>
     );
