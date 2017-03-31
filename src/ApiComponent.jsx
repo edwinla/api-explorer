92c4eaddf768eba33c25cf6a32cdaa7342b1178a
laed37 <La.Edwin37@gmail.com>
2017-03-31 10:49:59 -0700

    Fix fetch promise not catching response

--- src/ApiComponent.jsx
+++ src/ApiComponent.jsx
@@ -52,10 +52,9 @@ class ApiComponent extends Component  {
       body: filterObjectsWithKey(this.state.data, 'attributes')
     };
 
-    // fetchRequest(requestParams)
-    //   .then(response => this.setState({response}));
     fetchRequest(requestParams)
-      .then(response => this.setState({response: response.json()}))
+      .then(success => success.json())
+      .then((promise) => this.setState({response: {[resource]: promise}}))
       .catch(error => this.setState({response: error.message}));
   }
 
